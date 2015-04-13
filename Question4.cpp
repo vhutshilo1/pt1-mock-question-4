3104ad73f78eeead9f2b0f1f1aa47f0bdadb7f9f
John Poole <poolej@ukzn.ac.za>
2015-04-13 09:17:03 +0200

    add question 4

---
+++ Question4.cpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//                         COMPUTER METHODS 3 PRACTICAL TEST 1 (MOCK)
+// Name:
+// Student No:
+// Date: 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ***** QUESTION 4 ****
+// Question 4 has 3 parts (you do NOT need to include each class in a separate file!)
+// 4.1 Create a virtual base class "Animal" which has two methods, name() and does(). 
+// 4.2 From class "Animal", derive a concrete class "Bird" and "Dog".  Print appropriate message for
+//     each method.
+// 4.3 Write a driver program which creates an object of each type and demonstrates POLYMORPHISM. 
